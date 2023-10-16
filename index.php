3780ffb170e84f297166f9dd8576d3742453e3ea
MedAmine53 <temmaramine930@gmail.com>
2023-10-16 03:06:22 +0100

    R/1

--- index.php
+++ index.php
@@ -7,5 +7,7 @@ use App\Controllers\EventController as EventController;
 
 // Categories
 use App\Controllers\CategoryController as CategoryController;
-$category = new CategoryController; 
-$category->index();
+// $category = new CategoryController; 
+// $category->index();
+
+print_r(parse_url($_SERVER['REQUEST_URI'])) ;
